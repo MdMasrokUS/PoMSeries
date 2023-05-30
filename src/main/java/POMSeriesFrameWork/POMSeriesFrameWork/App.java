4c8a18c0cda7321e670f2ed8f75b7f6ebe9f87a7
Naheed <nahidns08@gmail.com>
2023-05-29 22:35:48 -0400

    first commit

---
+++ src/main/java/POMSeriesFrameWork/POMSeriesFrameWork/App.java
@@ -0,0 +1,13 @@
+package POMSeriesFrameWork.POMSeriesFrameWork;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
